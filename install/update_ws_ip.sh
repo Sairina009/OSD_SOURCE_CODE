390dd8655ba457ad3a0641877af006e989497727
Sairina009 <104815187+Sairina009@users.noreply.github.com>
2024-02-28 11:56:31 +0530

    Update update_ws_ip.sh

--- install/update_ws_ip.sh
+++ install/update_ws_ip.sh
@@ -4,7 +4,6 @@
 new_ip_address="your_desired_ip_address"
 
 # Replace the IP address in the file
-sed -i "s/ws:\/\/192\.168\.0\.108:9999/ws:\/\/$new_ip_address:9999/g" /var/www/html/od/in.html
-sed -i "s/ws:\/\/192\.168\.0\.108:9999/ws:\/\/$new_ip_address:9999/g" /var/www/html/od/in_user.html
+sed -i "s/ws:\/\/192\.168\.1\.10:9999/ws:\/\/$new_ip_address:9999/g" /var/www/html/od/index.html
 
 echo "WebSocket URL updated to ws://$new_ip_address:9999"
