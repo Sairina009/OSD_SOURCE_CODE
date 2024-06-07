5f5742fdcaddb2deb2c13cf8b4dcc5bdca560e30
Sairina009 <104815187+Sairina009@users.noreply.github.com>
2024-06-07 12:03:35 +0530

    Update funtionheader.php

--- funtionheader.php
+++ funtionheader.php
@@ -2,7 +2,7 @@
 <?php
 $updatedata=new DB_con();
 if (isset($_POST['hid'])) { 
-    for ($u=1 ; $u<=8; $u++) {
+    for ($u=1 ; $u<=2; $u++) {
         if (isset($_POST['user' . $u])) {
             $value =$_POST['user' . $u];
         } else {
@@ -10,18 +10,10 @@ if (isset($_POST['hid'])) {
         }
          $sql=$updatedata->hid($value, $u); 
     }
-    for ($u=9 ; $u<=16; $u++) {
-        if (isset($_POST['user1' . $u])) {
-            $value =$_POST['user1' . $u];
-        } else {
-            $value= '';
-        }
-        $sql=$updatedata->hid($value, $u);  
-    }
 }
 
 if (isset($_POST['mass'])) { 
-    for ($m=1 ; $m<=8; $m++) {
+    for ($m=1 ; $m<=2; $m++) {
         if (isset($_POST['mass' . $m])) {
             $value =$_POST['mass' . $m];
         } else {
@@ -29,18 +21,11 @@ if (isset($_POST['mass'])) {
         }
         $sql=$updatedata->mass($value, $m); 
     }
-    for ($m=9 ; $m<=16; $m++) {
-        if (isset($_POST['mass1' . $m])) {
-            $value =$_POST['mass1' . $m];
-        } else {
-            $value= '';
-        }
-        $sql=$updatedata->mass($value, $m); 
-    }
+
 } 
 
 if (isset($_POST['card'])) { 
-    for ($c=1 ; $c<=8; $c++) {
+    for ($c=1 ; $c<=2; $c++) {
         if (isset($_POST['card' . $c])) {
             $value =$_POST['card' . $c];
         } else {
@@ -48,18 +33,11 @@ if (isset($_POST['card'])) {
         }
         $sql=$updatedata->card($value, $c); 
     }
-    for ($c=9 ; $c<=16; $c++) {
-        if (isset($_POST['card1' . $c])) {
-            $value =$_POST['card1' . $c];
-        } else {
-            $value= '';
-        }
-        $sql=$updatedata->card($value, $c); 
-    }
+
 }
 
 if (isset($_POST['virtual'])) { 
-    for ($v=1 ; $v<=8; $v++) {
+    for ($v=1 ; $v<=2; $v++) {
         if (isset($_POST['virtual' . $v])) {
             $value =$_POST['virtual' . $v];
         } else {
@@ -67,14 +45,7 @@ if (isset($_POST['virtual'])) {
         }
         $sql=$updatedata->virtua($value, $v);  
     }
-    for ($v=9 ; $v<=16; $v++) {
-        if (isset($_POST['virtual1' . $v])) {
-            $value =$_POST['virtual1' . $v];
-        } else {
-            $value= '';
-        }
-        $sql=$updatedata->virtua($value, $v); 
-    }
+
 }
 ?>
 <!DOCTYPE html>
@@ -97,9 +68,7 @@ if (isset($_POST['virtual'])) {
                     <th class=lbl style=width:55px><?php echo $lang['A19']; ?></th>
                     <th class=lbl style=width:400px><?php echo $lang['F5']; ?></th>
                      <th style=background-color:lightsteelblue;text-align:left>
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;U01&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;U02&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;U03&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;U04&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;U05&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;U06&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;U07&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;U08
-                        <br>
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;U09&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;U10&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;U11&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;U12&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;U13&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;U14&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;U15&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;U16
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;U01&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;U02&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     </th>
                     <th class=lbl style=width:90px><?php echo $lang['B11']; ?></th>
                 </tr>
